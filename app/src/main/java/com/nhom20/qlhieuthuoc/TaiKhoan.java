f5a09bae2b75f135530763a204309ca646e6d756
Tai <phuoctai12t@gmail.com>
2022-04-03 23:44:00 +0700

    update user

--- app/src/main/java/com/nhom20/qlhieuthuoc/TaiKhoan.java
+++ app/src/main/java/com/nhom20/qlhieuthuoc/TaiKhoan.java
@@ -9,27 +9,26 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-public class TaiKhoan extends AppCompatActivity {
+import java.util.Locale;
 
-    EditText edtUsername, edtPassword, edtEmail;
+public class TaiKhoan extends AppCompatActivity {
+    EditText edtUsername, edtEmail;
     Button btnSave;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tai_khoan);
         addControls();
-
     }
+
     private void addControls() {
-        edtUsername = (EditText) findViewById(R.id.edtUsername);
-        edtPassword = (EditText) findViewById(R.id.edtPassword);
-        edtEmail = (EditText) findViewById(R.id.edtEmail);
-        btnSave = (Button) findViewById(R.id.btnSaveInfoAccount);
+        edtUsername = findViewById(R.id.edtUsername);
+        edtEmail =  findViewById(R.id.edtEmail);
+        btnSave = findViewById(R.id.btnSaveInfoAccount);
     }
 
     public void handBack(View view) {
@@ -41,38 +40,35 @@ public class TaiKhoan extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         edtUsername.setEnabled(false);
-        SharedPreferences preferences = getSharedPreferences(Login.nameShare , MODE_PRIVATE);
-        String user = preferences.getString("username","");
-        Login.database= openOrCreateDatabase(Login.DATABASE_NAME, MODE_PRIVATE, null);
+        SharedPreferences preferences = getSharedPreferences(Login.nameShare, MODE_PRIVATE);
+        String user = preferences.getString("username", "");
+        Login.database = openOrCreateDatabase(Login.DATABASE_NAME, MODE_PRIVATE, null);
         Cursor cursor = Login.database.query("USER", null, "USERNAME =?",
                 new String[]{user}, null, null, null);
-        String email="",password="";
-        if(cursor.moveToNext()){
+        String email = "";
+        if (cursor.moveToNext()) {
             email = cursor.getString(1);
-            password = cursor.getString(2);
-
-
         }
         cursor.close();
 
         edtUsername.setText(user);
-        edtPassword.setText(password);
         edtEmail.setText(email);
-
-
     }
 
     public void handleSave(View view) {
-        String user = edtUsername.getText().toString();
-        String password = edtPassword.getText().toString();
-        String email = edtEmail.getText().toString();
-        ContentValues values = new ContentValues();
-        values.put("EMAIL" , email);
-        values.put("PASSWORD" , password);
-        int kq = Login.database.update("USER", values, "USERNAME = ?", new String[]{user});
-        if(kq>0){
-            Toast.makeText(this, "Lưu thông tin thành công",Toast.LENGTH_SHORT).show();
-            finish();
-        }else Toast.makeText(this, "Lưu thông tin thất bại",Toast.LENGTH_SHORT).show();
+        String username = edtUsername.getText().toString();
+        String email = edtEmail.getText().toString().toLowerCase(Locale.ROOT).trim();
+
+        if (!SignUpActivity.isEmail(email)) {
+            Toast.makeText(this, "Email không hợp lệ", Toast.LENGTH_SHORT).show();
+        } else {
+            ContentValues values = new ContentValues();
+            values.put("EMAIL", email);
+            int kq = Login.database.update("USER", values, "USERNAME = ?", new String[]{username});
+            if (kq > 0) {
+                Toast.makeText(this, "Lưu thông tin thành công", Toast.LENGTH_SHORT).show();
+            } else Toast.makeText(this, "Lưu thông tin thất bại", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
